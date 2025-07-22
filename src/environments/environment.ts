8f67f9f3c5cda759c293df97416fbaf570313232
David Duque <daviddqo123@gmail.com>
2025-07-22 18:42:22 -0500

    se actualizo el servidor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseAPIURL: 'http://localhost:3000/api/'
-    // baseAPIURL: 'https://e-commerce-back-eta.vercel.app/api/'
+    // baseAPIURL: 'http://localhost:3000/api/'
+    baseAPIURL: 'https://e-commerce-back-eta.vercel.app/api/'
   };
   
